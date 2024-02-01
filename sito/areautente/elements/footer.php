e2690550f733fe7ea22bdcd3a0d6095e7b927f4f
nickpavesi <nicolomichelepavesi@gmail.com>
2024-02-01 22:52:42 +0100

    Update footer.php

--- sito/areautente/elements/footer.php
+++ sito/areautente/elements/footer.php
@@ -1,11 +1,12 @@
+
   <div class="container-fluid footer">
-    <div class="copy">
-      <div class="col-md-3 nome">Mercatino dei Libri Usati</div>
-      <address class="col-md-6 nome">
-	  Consultorio familiare diocesano Insieme &bull; Tel: +39 0373 82723 &bull;
-	  Mail: <?php echo getMail(); ?><br>
-	  Sede consegna/ritiro I.I.S. G.Galilei - Aula Ristoro - Via Canossa 21 - Crema (CR)
-      </address>
-      <div class="col-md-3 io">P.D. & D.P.</div>
-    </div>
+  <div class="row">
+    <div class="col-md-3 nome">Mercatino dei Libri Usati</div>
+    <address class="col-md-6 nome">
+      Consultorio familiare diocesano Insieme &bull; Tel: +39 0373 82723 &bull;
+      Mail: <?php echo getMail(); ?><br>
+      Sede consegna/ritiro I.I.S. G.Galilei - Aula Ristoro - Via Canossa 21 - Crema (CR)
+    </address>
+    <div class="col-md-3 io">P.D. & D.P.</div>
   </div>
+</div>
